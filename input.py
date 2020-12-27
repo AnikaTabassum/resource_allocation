349fc8dbc1442d07d48fd1e26b6eaf927d364626
AnikaTabassum <35559892+AnikaTabassum@users.noreply.github.com>
2020-12-28 00:55:50 +0600

    critical path--- done

--- input.py
+++ input.py
@@ -43,13 +43,37 @@ class inputProcessing:
 					start_activities.append(x[0])
 				pred.append(x[i])
 
-			ac1=Activity(x[0],x[len(x)-2],x[len(x)-1],-1,-1,-1,-1,-1,pred,sucs)
+			ac1=Activity(x[0],x[len(x)-2],x[len(x)-1],-1,-1,9874217,9874217,-1,pred,sucs)
 			all_activity.append(ac1)
 		## if there are more than 1 start nodes, make a start node on your own
 		if cnt>1:
 			all_activity=inputProcessing().create_start_node(start_activities, all_activity)
+		else:
+			all_activity=inputProcessing().rename_start_node(start_activities, all_activity)
+
 		# print(all_activity[0].name)
-		self.forward_pass(all_activity)
+		all_activity=self.forward_pass(all_activity)
+		print("after fp ", all_activity)
+		all_activity=self.backward_pass(all_activity)
+		print(all_activity)
+		self.get_critical_path(all_activity)
+
+	def rename_start_node(self, start_activities, all_activity):
+		sucs_list=[]
+		all_activity=self.get_successor_list(all_activity)
+		for activity in all_activity:
+			if activity.name==start_activities[0]:
+				activity.name="start"
+				activity.es=0
+				activity.ef=activity.es+activity.duration
+				sucs_list=activity.sucs
+		print("sucs ", sucs_list)
+		for activity in all_activity:
+			if activity.name in sucs_list:
+				activity.pred=["start"]
+
+		return all_activity
+
 
 	def create_start_node(self, start_activities, all_activity):
 		pred=[]
@@ -69,7 +93,7 @@ class inputProcessing:
 		
 		return all_activity
 
-	def bfs(self,visited,queue, graph, node, all_activity):
+	def bfs_forward_pass(self,visited,queue, graph, node, all_activity):
 		visited.append(node)
 		queue.append(node)
 		# print(graph)
@@ -100,9 +124,54 @@ class inputProcessing:
 
 		return all_activity
 
-	# def forward_pass_calculation(self, activity_dict):
+	def bfs_backward_pass(self,visited,queue, graph, node, all_activity):
+		visited.append(node)
+		queue.append(node)
+		# print(graph)
+		## dictionary for mapping the activity names with the activity objects easily
+		activity_dict={}
+		for activity in all_activity:
+			activity_dict[activity.name]=activity
+			# print(activity.name)
 
 
+		while queue:
+			s = queue.pop(0) 
+			print ("lol ",s, end = " ") 
+			# print(graph)
+			for neighbour in graph[s]:
+				if neighbour!="-":
+					#### visiting each neighbour and calculating LS and LF
+					############ parallel path gular level equivalent na hole jhamela---------
+					activity_dict[neighbour].lf=min(activity_dict[s].ls,activity_dict[neighbour].lf)
+					activity_dict[neighbour].ls=activity_dict[neighbour].lf-activity_dict[neighbour].duration
+					# print("neighbour ",neighbour, activity_dict[neighbour].lf)
+					if neighbour not in visited:
+						visited.append(neighbour)
+						queue.append(neighbour)
+		#### we were keeping values in the dict before, nnow keepingthis in the main list
+		for activity in all_activity:
+			activity.ls=activity_dict[activity.name].ls
+			activity.lf=activity_dict[activity.name].lf
+		# return all_activity
+		print("sghHDFGDFS",all_activity[0].name)
+
+		return all_activity
+	def get_successor_list(self, all_activity):
+		pred_dict={}
+		for activity in all_activity:
+			pred_dict[activity.name]=activity.pred
+
+		for activity in all_activity:
+			sucs_list=[]
+			for key in pred_dict:
+				# print(pred_dict[key])
+				if activity.name in pred_dict[key]:
+					sucs_list.append(key)
+					# sucs_dict[activity.name]=key
+			# sucs_dict[activity.name]=sucs_list
+			activity.sucs=sucs_list
+		return all_activity
 	def forward_pass(self,all_activity):
 		# all_activity
 		############ mapping activities with predecessors#####################
@@ -122,6 +191,10 @@ class inputProcessing:
 		for activity in all_activity:
 			if activity.name=="start":
 				activity.sucs=start_sucs
+		for activity in all_activity:
+			for val in start_sucs:
+				if activity.name==val:
+					activity.pred=['start']
 
 		sucs_dict={}### mapping each node with it's successors list
 		cnt=0
@@ -152,7 +225,7 @@ class inputProcessing:
 		queue = []     #Initialize a queue
 
 		############# calling cfs with start node###############
-		all_activity=inputProcessing().bfs(visited, queue, sucs_dict, 'start', all_activity)
+		all_activity=inputProcessing().bfs_forward_pass(visited, queue, sucs_dict, 'start', all_activity)
 
 		
 
@@ -168,9 +241,41 @@ class inputProcessing:
 
 
 		################ printing all the activities#####################
+		# for activity in all_activity:
+		# 	print("activity ", activity.name, activity.sucs, activity.pred, activity.es, activity.ef)
+		return all_activity
+	def backward_pass(self, all_activity):
+		pred_dict={}
 		for activity in all_activity:
-			print("activity ", activity.name, activity.sucs, activity.pred, activity.es, activity.ef)
+			pred_dict[activity.name]=activity.pred
+			if activity.name=="finish":
+				activity.lf=activity.ef
+				activity.ls=activity.lf
+		print(pred_dict)
+		visited = [] # List to keep track of visited nodes.
+		queue = []     #Initialize a queue
 
+		############# calling bfs with start node###############
+		all_activity=inputProcessing().bfs_backward_pass(visited, queue, pred_dict, 'finish', all_activity)
+
+		################ printing all the activities#####################
+		for activity in all_activity:
+			print("activity ", activity.name, activity.sucs, activity.pred, activity.es, activity.ef,  activity.ls,  activity.lf)
+
+		print(all_activity[0].name)
+		return all_activity
+
+	def get_critical_path(self, all_activity):
+		critical_path=[]
+		print(all_activity)
+		for activity in all_activity:
+			if activity.ls-activity.es==0:
+				activity.freefloat=0
+				critical_path.append(activity.name)
+			else:
+				activity.freefloat= activity.ls-activity.es
+
+		print(critical_path)
 
 inputProcessing().takeinput()
 # inputProcessing().forward_pass()
